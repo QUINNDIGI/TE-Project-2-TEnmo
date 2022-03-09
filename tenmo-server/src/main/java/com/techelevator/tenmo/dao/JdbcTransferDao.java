198ab65b4f664855ef4988e3ed9ea8ce0e45b57e
Susanne Clark <susanneaclark@gmail.com>
2022-03-09 16:49:50 -0500

    dao transfer table

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -1,4 +1,22 @@
 package com.techelevator.tenmo.dao;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.math.BigDecimal;
+
 public class JdbcTransferDao {
+
+    private Long transferId;
+    private Long transferTypeId;
+    private Long transferStatusId;
+    private Long accountFrom;
+    private Long accountTo;
+    private BigDecimal amount;
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
 }
