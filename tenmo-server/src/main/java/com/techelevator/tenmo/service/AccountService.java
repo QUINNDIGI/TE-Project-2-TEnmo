4b54a1bceeedd54bc430651dcfc8371fb3501ace
Susanne Clark <susanneaclark@gmail.com>
2022-03-09 17:02:33 -0500

    Account service

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
@@ -0,0 +1,12 @@
+package com.techelevator.tenmo.service;
+
+import java.math.BigDecimal;
+
+public class AccountService {
+
+    public BigDecimal getBalance()
+    {
+        BigDecimal balance = new BigDecimal ("1000.00");
+        return balance;
+    }
+}
