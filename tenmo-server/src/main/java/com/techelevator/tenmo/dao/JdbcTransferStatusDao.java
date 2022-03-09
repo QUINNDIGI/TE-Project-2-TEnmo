d18063e79b0e6a2352920dd66ee505cc7f133dd5
Susanne Clark <susanneaclark@gmail.com>
2022-03-09 16:50:51 -0500

    Dao object

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
@@ -1,4 +1,16 @@
 package com.techelevator.tenmo.dao;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class JdbcTransferStatusDao {
+
+    private Long transferStatusId;
+    private String transferStatusDescription;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferStatusDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
