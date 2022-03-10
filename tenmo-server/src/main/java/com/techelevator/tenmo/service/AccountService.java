833008b6a80b0a1593eaad47e8ff79a109e19cbb
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 18:43:36 -0500

    Removed commented code

--- tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
@@ -36,53 +36,7 @@ public class AccountService {
         User user = userDao.findByUsername(username);
         Long userId = user.getId();
         balance = accountDao.getBalance(userId);
-        //System.out.println(userInfo.getName());
+
         return balance;
     }
-/*
-    private Meme[] getMemes() {
-
-        // add code to call get_memes endpoint
-        try {
-            MemeApiGetResponse response = restTemplate.getForObject(API_BASE_URL + "get_memes", MemeApiGetResponse.class);
-
-            return response.getData().getMemes();
-        } catch (RestClientResponseException ex) {
-            BasicLogger.log("API Error: " + ex.getRawStatusCode() + " " + ex.getStatusText());
-        } catch (ResourceAccessException ex) {
-            BasicLogger.log("API Error: " + ex.getMessage());
-        }
-        return new Meme[] {};
-    }*/
-
-    /**
-     * Uses caption_image endpoint to create meme and return URL
-     * @param info
-     * @return String
-     */
-    /*public String getBalance(CreateMemeInfo info) {
-
-        CaptionedMeme newMeme = new CaptionedMeme();
-
-        newMeme.setTemplateId(info.getMemeId());
-        newMeme.setText0(info.getCaption());
-        newMeme.setUsername(API_USERNAME);
-        newMeme.setPassword(API_PASSWORD);
-
-        MultiValueMap<String, String> postData = memeUtils.formDataFromCaptionedMeme(newMeme);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(postData, httpHeaders);
-
-
-        MemeApiCreateResponse response = restTemplate.postForObject(API_BASE_URL + "caption_image", entity, MemeApiCreateResponse.class);
-
-        return response.getData().getUrl();
-    }*/
-
-
-
-
 }
