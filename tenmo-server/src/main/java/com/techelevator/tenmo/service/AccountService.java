4755d0babe11e1589c829246db713ef58a1c2eb9
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 18:04:17 -0500

    getBalance working

--- tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.techelevator.tenmo.service;
 
+import com.techelevator.tenmo.dao.JdbcAccountDao;
+import com.techelevator.tenmo.dao.JdbcUserDao;
+import com.techelevator.tenmo.model.User;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.ResourceAccessException;
@@ -11,13 +15,28 @@ import org.springframework.web.client.RestClientResponseException;
 import java.math.BigDecimal;
 import java.security.Principal;
 
+
 @Service
 public class AccountService {
 
+    private JdbcUserDao userDao;
+    private JdbcAccountDao accountDao;
+
+    public AccountService (JdbcUserDao userDao, JdbcAccountDao accountDao)
+    {
+
+        this.userDao = userDao;
+        this.accountDao = accountDao;
+    }
     public BigDecimal getBalance(Principal userInfo)
     {
-        BigDecimal balance = new BigDecimal ("1000.00");
-        System.out.println(userInfo.getName());
+        BigDecimal balance = new BigDecimal ("500.00");
+
+        String username = userInfo.getName();
+        User user = userDao.findByUsername(username);
+        Long userId = user.getId();
+        balance = accountDao.getBalance(userId);
+        //System.out.println(userInfo.getName());
         return balance;
     }
 /*
