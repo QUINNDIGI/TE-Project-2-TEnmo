c46f3fd7ca7413b62cd10af2c4b815a416a480a1
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 18:00:00 -0500

    GetBalance Working

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -2,10 +2,8 @@ package com.techelevator.tenmo.services;
 
 
 import com.techelevator.tenmo.model.Account;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import com.techelevator.tenmo.model.AuthenticatedUser;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
@@ -17,27 +15,34 @@ import java.math.BigDecimal;
 public class AccountService {
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final String baseUrl;
 
-    public AccountService()
+    public AccountService(String baseUrl)
     {
 
+        this.baseUrl = baseUrl;
     }
 
-    public BigDecimal getBalance() {
+    public BigDecimal getBalance(AuthenticatedUser user) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Account> entity = new HttpEntity<>(Account, headers);
-
-        Account returnedAccount = null;
+        headers.setBearerAuth(user.getToken());
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        BigDecimal balance;
+        //Account returnedAccount = null;
         try {
-            returnedAccount  = restTemplate.postForObject( API_BASE_URL +"/tenmo/" + "getBalance", entity, Account.class);
-            return returnedAccount.getBalance();
+
+        balance  = restTemplate.exchange(baseUrl +"/tenmo/" + "getBalance", HttpMethod.GET, entity, BigDecimal.class).getBody();
+
+            //).postForObject( API_BASE_URL +"/tenmo/" + "getBalance", entity, Account.class);
+            return balance;
         } catch (RestClientResponseException | ResourceAccessException e) {
             com.techelevator.util.BasicLogger.log(e.getMessage());
         }
-        return returnedAccount;
+        return null;
         //BigDecimal balance = new BigDecimal("1000.00");
         //System.out.println("Current Balance: " );
 
+        //return null;
     }
 }
