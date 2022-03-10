0b6ed2106e55ec8b432b42a1b7ca1d6370a890f1
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 10:22:17 -0500

    Added Java Bean Validation

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -2,14 +2,18 @@ package com.techelevator.tenmo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class Account {
 
     @JsonProperty("account_id")
+    @NotNull
     private Long accountId;
+    @NotNull
     @JsonProperty("user_id")
     private Long userId;
+    @NotNull
     private BigDecimal balance;
 
 
