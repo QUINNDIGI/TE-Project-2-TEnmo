d59d9b925f9608c3aedf906fc190420e34dc2615
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 10:27:58 -0500

    Took out preauth

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -4,6 +4,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class TransferController {
 }
