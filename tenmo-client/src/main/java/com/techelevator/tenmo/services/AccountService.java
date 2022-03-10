e76e6200587efaf3a12172aa20a96fc1c07a3956
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 14:59:49 -0500

    Working on getBalance

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -2,8 +2,13 @@ package com.techelevator.tenmo.services;
 
 
 import com.techelevator.tenmo.model.Account;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -19,9 +24,20 @@ public class AccountService {
     }
 
     public BigDecimal getBalance() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Account> entity = new HttpEntity<>(Account, headers);
 
-        BigDecimal balance = new BigDecimal("1000.00");
-        System.out.println("Current Balance: " );
-        return balance;
+        Account returnedAccount = null;
+        try {
+            returnedAccount  = restTemplate.postForObject( API_BASE_URL +"/tenmo/" + "getBalance", entity, Account.class);
+            return returnedAccount.getBalance();
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            com.techelevator.util.BasicLogger.log(e.getMessage());
+        }
+        return returnedAccount;
+        //BigDecimal balance = new BigDecimal("1000.00");
+        //System.out.println("Current Balance: " );
+
     }
 }
