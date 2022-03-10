bff84a830595551bf5028ef87b0563a0cf8e0d8b
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 10:23:44 -0500

    Added Java Bean Validation

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferType.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferType.java
@@ -2,10 +2,14 @@ package com.techelevator.tenmo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
+
 public class TransferType {
 
+    @NotNull
     @JsonProperty("transfer_type_id")
     private Long transferTypeId;
+    @NotNull
     @JsonProperty("transfer_type_desc")
     private String transferTypeDescription;
 
