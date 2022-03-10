84d7ca271e60ad0c6c2a781e02c379b2d011f986
Susanne Clark <susanneaclark@gmail.com>
2022-03-10 18:02:41 -0500

    GetBalance working

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -4,9 +4,10 @@ import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.User;
 
 import java.math.BigDecimal;
+import java.security.Principal;
 import java.util.List;
 
 public interface AccountDao {
-    public BigDecimal getBalance(Long accountId, Long userId);
+    public BigDecimal getBalance(Long userId);
 
 }
