ddd50667d4882b92d2cfc9d7db736d651df1a6e3
Susanne Clark <susanneaclark@gmail.com>
2022-03-11 14:44:23 -0500

    Working on transfer

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,14 +1,17 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.JdbcTransferDao;
+import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.User;
 import com.techelevator.tenmo.service.AccountService;
 import com.techelevator.tenmo.service.TransferService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.security.Principal;
 import java.util.ArrayList;
@@ -34,5 +37,13 @@ public class TransferController {
         userList = transferService.listUsers(userInfo);
 
         return userList;
+    }
+
+    @RequestMapping(value = "/transfer", method = RequestMethod.POST)
+    public Transfer makeTransfer(@RequestParam  @Valid Long transferFrom, @RequestParam @Valid int transferTo, @RequestParam @Valid BigDecimal amount) {
+
+        Transfer returnedTransfer = transferService.makeTransfer(transferFrom, transferTo, amount);
+
+        return returnedTransfer;
     }
 }
