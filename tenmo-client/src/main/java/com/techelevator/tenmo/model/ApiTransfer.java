120ffa468616880ef3313b71628ece1f8071b808
Susanne Clark <susanneaclark@gmail.com>
2022-03-12 11:14:42 -0500

    Working on transfer

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/ApiTransfer.java
@@ -0,0 +1,101 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class ApiTransfer {
+
+
+    private Long transferId;
+    private Long transferTypeId;
+    private Long transferStatusId;
+    private Long accountFrom;
+    private Integer accountTo;
+    private BigDecimal amount;
+    private String transferStatusDesc;
+    private String toUsername;
+    private String fromUsername;
+    private String transferTypeDesc;
+
+    public Long getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(Long transferId) {
+        this.transferId = transferId;
+    }
+
+    public Long getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(Long transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public Long getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(Long transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public Long getAccountFrom() {
+        return accountFrom;
+    }
+
+    public void setAccountFrom(Long accountFrom) {
+        this.accountFrom = accountFrom;
+    }
+
+    public Integer getAccountTo() {
+        return accountTo;
+    }
+
+    public void setAccountTo(Integer accountTo) {
+        this.accountTo = accountTo;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getTransferStatusDesc() {
+        return transferStatusDesc;
+    }
+
+    public void setTransferStatusDesc(String transferStatusDesc) {
+        this.transferStatusDesc = transferStatusDesc;
+    }
+
+    public String getToUsername() {
+        return toUsername;
+    }
+
+    public void setToUsername(String toUsername) {
+        this.toUsername = toUsername;
+    }
+
+    public String getFromUsername() {
+        return fromUsername;
+    }
+
+    public void setFromUsername(String fromUsername) {
+        this.fromUsername = fromUsername;
+    }
+
+    public String getTransferTypeDesc() {
+        return transferTypeDesc;
+    }
+
+    public void setTransferTypeDesc(String transferTypeDesc) {
+        this.transferTypeDesc = transferTypeDesc;
+    }
+
+
+
+}
